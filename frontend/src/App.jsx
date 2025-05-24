77c7510c7c225c174cc1deed0c6006d0410d0b4d
asheefb <ashifkb80629@gmail.com>
2025-05-24 19:50:04 +0530

    Added Exception handler for Field level Validation.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import RegisterPage from './pages/RegisterPage';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <RegisterPage />
-    </div>
+    <>
+    <Navbar/>
+    
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/signup" element={<RegisterPage />} />
+      {/* <Route path="/register" element={<RegisterPage />} /> */}
+
+    </Routes>
+    </>
   );
 }
 
